2790bd8121e12460169a6dab2682941f0691981e
Alex Motzenbecker <alex.motezenbecker@ibm.com>
2024-09-12 15:44:32 -0400

    fix: remove click handler for 'skip' button

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # Video Ad blocker Chrome Extension
 
-Fast-forwards the video ads at 16x time. Runs continuously in the background for specified domain.
+v1.1.0
+
+Skips the video ads. Runs continuously in the background for specified domain.
 If an ad is found, it forwards to the end, waits a moment and clicks on the skip button.
 
 ## Installation
 
-In Chrome, go to:
-
 ```
-Settings > Extensions > Manage Extensions
+Setting > Extensions > Manage Extensions
 ```
 
 Toggle "Developer mode" **ON** in the top right corner.
 
 ```
- Load unpacked > Select this folder you downloaded.
+ Load unpacked > Select this folder
 ```
--- skip-ads.js
+++ skip-ads.js
@@ -1,23 +1,23 @@
 if (window.ffIntId) {
-  clearInterval(window.ffIntId);
+    clearInterval(window.ffIntId);
 }
 
+let fastForwarding = false;
+
 window.ffIntId = setInterval(() => {
-  const ad = [...document.querySelectorAll('.ad-showing')][0];
-  if (ad != null) {
-    const video = document.querySelector('video');
-    // fast forward to the end of the ad
-    if (!isNaN(video.duration) && video.duration > 0) {
-      console.log('Ad duration:', video.duration);
-      video.currentTime = video.duration;
+    if (!fastForwarding){
+        const ad = [...document.querySelectorAll('.ad-showing')][0];
+        if (ad != null) {
+            const video = document.querySelector('video');
+            // fast forward ad
+            if (video) {
+                video.playbackRate = 16; 
+                fastForwarding = true;
+                clearTimeout(window.clearFFTimeoutId);
+                window.clearFFTimeoutId = setTimeout(() => {
+                    fastForwarding = false;
+                }, 5000);
+            }
+        }
     }
-    setTimeout(() => {
-      const skipButton = document.querySelector('.ytp-ad-skip-button-modern');
-      // wait a moment, then click on the skip button
-      if (skipButton != null) {
-        skipButton.click();
-        console.log(`Skipped ad.`);
-      }
-    }, 500);
-  }
-}, 650);
+}, 1000);
